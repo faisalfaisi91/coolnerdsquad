ddcf574c923897329f4b28c540893b912086b649
VenD <vend@192.168.0.117>
2020-07-26 14:16:25 +0500

    Updated code structure and added new plugins

--- wp-config.php
+++ wp-config.php
@@ -26,10 +26,10 @@ define( 'DB_NAME', 'coolnerd' );
 define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', '123456' );
+define( 'DB_PASSWORD', '12345' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', '127.0.0.1' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
